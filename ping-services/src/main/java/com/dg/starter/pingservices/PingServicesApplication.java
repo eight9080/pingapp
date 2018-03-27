05f8cdb037999e24b97bb9b1b73e743414fdb95c
eight9080 <eight9080@gmail.com>
2018-03-28 01:39:25 +0300

    initial commit

---
+++ ping-services/src/main/java/com/dg/starter/pingservices/PingServicesApplication.java
@@ -0,0 +1,12 @@
+package com.dg.starter.pingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingServicesApplication.class, args);
+	}
+}
